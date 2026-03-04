c4a1bc0ef801c4a41f10eb71f71eeb2ab41cfdd7
Spewep <arcgabrielsantos@gmail.com>
2026-03-04 18:54:51 -0300

    Camera Fix
    Add camera colider
    bottle mode vision fix
    moves player only in normal mode

--- Procedural animation test/Assets/Scripts/Camera/CameraControle.cs
+++ Procedural animation test/Assets/Scripts/Camera/CameraControle.cs
@@ -19,9 +19,11 @@ public class CameraController : MonoBehaviour
 
     private float yaw;
     private float pitch;
+    private Moviment mov;
 
     void Start() // alnha e locka o mouse
     {
+        mov = player.GetComponentInParent<Moviment>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
@@ -32,19 +34,29 @@ public class CameraController : MonoBehaviour
 
     void Update() // coloca rotacao 
     {
+        if (cameraTarget == null || lookAction == null)
+            return;
+
         Vector2 lookInput = lookAction.action.ReadValue<Vector2>();
 
         bool isMouse = Mouse.current != null && Mouse.current.delta.ReadValue() != Vector2.zero;
-
         float sensitivity = isMouse ? mouseSensitivity : controllerSensitivity * Time.deltaTime;
 
         yaw += lookInput.x * sensitivity;
         pitch -= lookInput.y * sensitivity;
-
         pitch = Mathf.Clamp(pitch, minVerticalAngle, maxVerticalAngle);
 
         cameraTarget.rotation = Quaternion.Euler(pitch, yaw, 0);
 
-        player.rotation = Quaternion.Euler(0, yaw, 0);
+        if (mov != null && !mov.BottleMode && mov.currentInput.magnitude > 0.1f)
+        {
+            Rigidbody rb = mov.GetComponent<Rigidbody>();
+
+            if (rb != null && !mov.BottleMode && mov.currentInput.magnitude > 0.1f)
+            {
+                Quaternion targetRot = Quaternion.Euler(0, yaw, 0);
+                rb.MoveRotation(targetRot);
+            }
+        }
     }
 }
