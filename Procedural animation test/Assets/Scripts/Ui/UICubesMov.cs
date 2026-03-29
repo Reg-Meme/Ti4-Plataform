69c1dda1f792cb1953b649bed35488e3fde9338a
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2026-03-29 18:24:32 -0300

    vishimaria

--- Procedural animation test/Assets/Scripts/Ui/UICubesMov.cs
+++ Procedural animation test/Assets/Scripts/Ui/UICubesMov.cs
@@ -2,53 +2,37 @@ using UnityEngine;
 
 public class UICubesMov : MonoBehaviour
 {
-     [Header("Círculo Principal")]
-    public float raio = 5f;
-    public float velocidadeOrbita = 2f;
+    public float Ray;
+    public float Spd;
     public enum Eixo { XY, XZ, YZ }
-    public Eixo planoPrincipal = Eixo.XZ;
+    public Eixo MainSpin;
+    public float SubRay;
+    public float Spd2;
+    public Eixo SecSpin;
+    public float RotVel ;
+    private float Angle;
+    private float SubAngle;
 
-    [Header("Movimentação Extra (Círculo Secundário)")]
-    public float raioExtra = 1f;
-    public float velocidadeExtra = 5f;
-    public Eixo planoExtra = Eixo.XY;
-
-    [Header("Configurações de Rotação (Y)")]
-    public float velocidadeRotacaoY = 100f;
-    public bool sentidoHorario = true;
-
-    private float anguloPrincipal;
-    private float anguloExtra;
-    private Vector3 posicaoInicial;
 
     void Start()
     {
-        posicaoInicial = transform.position;
+        
     }
 
     void Update()
     {
-        // 1. Cálculo do Círculo Principal
-        anguloPrincipal += velocidadeOrbita * Time.deltaTime;
-        Vector3 offsetPrincipal = CalcularOffset(anguloPrincipal, raio, planoPrincipal);
-
-        // 2. Cálculo da Movimentação Extra (Segundo Círculo)
-        anguloExtra += velocidadeExtra * Time.deltaTime;
-        Vector3 offsetExtra = CalcularOffset(anguloExtra, raioExtra, planoExtra);
-
-        // Somamos a posição inicial + os dois círculos
-        transform.position = posicaoInicial + offsetPrincipal + offsetExtra;
-
-        // 3. Rotação em Y
-        float direcao = sentidoHorario ? 1f : -1f;
-        transform.Rotate(Vector3.up, velocidadeRotacaoY * direcao * Time.deltaTime);
+        Angle += Spd * Time.deltaTime;
+        Vector3 offset = CalcularOffset(Angle, Ray, MainSpin);
+        SubAngle += Spd2 * Time.deltaTime;
+        Vector3 SubOffset = CalcularOffset(SubAngle, SubRay, SecSpin);
+        transform.position = transform.position + offset + SubOffset;
+        transform.Rotate(new Vector3(0,1,0), RotVel * 1 * Time.deltaTime);
     }
 
-    // Função auxiliar para não repetir código de switch
-    Vector3 CalcularOffset(float tempo, float r, Eixo eixo)
+    Vector3 CalcularOffset(float Tim, float r, Eixo eixo)
     {
-        float cos = Mathf.Cos(tempo) * r;
-        float sin = Mathf.Sin(tempo) * r;
+        float cos = Mathf.Cos(Tim) * r;
+        float sin = Mathf.Sin(Tim) * r;
 
         switch (eixo)
         {
