3eb3b3443a0062ae297586c543ebe3be2e4e5761
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2026-04-12 12:33:48 -0300

    SecurityDoor&SfxManager

---
+++ Procedural animation test/Assets/Music&SFX/SfxManager.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SfXManager
+{
+
+    public enum SoundType
+
+    {
+
+        SecurityDoor,
+        
+
+    }
+
+    [ExecuteInEditMode]
+    public class soundManager : MonoBehaviour
+    {
+
+        [SerializeField] private SoundList[] soundList;
+        public static soundManager instance;
+       
+        public AudioSource musicSource;
+
+      
+
+        public void Start()
+        {
+           
+        }
+
+        public void Awake()
+        {
+            instance = this;
+        }
+
+        public static void PlaySound(SoundType sound, float volume = 1f)
+        {
+            AudioClip[] clips = instance.soundList[(int)sound].Sounds;
+            
+            AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
+            instance.musicSource.PlayOneShot(randomClip, volume);
+          
+            
+           
+            
+
+        }
+        public static void StopSound(SoundType sound, float volume = 1f)
+        {
+            AudioClip[] clips = instance.soundList[(int)sound].Sounds;
+
+            AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
+
+            instance.musicSource.Stop();
+
+        }
+#if UNITY_EDITOR
+        private void OnEnable()
+        {
+            string[] names = Enum.GetNames(typeof(SoundType));
+            Array.Resize(ref soundList, names.Length);
+            for (int i = 0; i < soundList.Length; i++)
+            {
+                soundList[i].name = names[i];
+            }
+        }
+    #endif
+    
+    }
+    
+    
+    [Serializable]
+    public struct SoundList
+    {
+        public AudioClip[] Sounds {get => sounds;}
+        [HideInInspector] public string name;
+        [SerializeField] public AudioClip[] sounds;
+    }
+}
