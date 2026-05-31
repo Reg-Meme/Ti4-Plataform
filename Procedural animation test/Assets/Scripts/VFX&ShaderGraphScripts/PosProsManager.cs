ff40143c0deae2d7bc0fa6735326f80f9eaec868
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2026-05-31 12:18:26 -0300

    Main Menu

---
+++ Procedural animation test/Assets/Scripts/VFX&ShaderGraphScripts/PosProsManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+public class PosProsManager : MonoBehaviour
+{
+   private Volume volume;
+    private ColorAdjustments colorAdjustments;
+    private ShadowsMidtonesHighlights GameColor;
+
+    void Awake()
+    {
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        volume = GetComponent<Volume>();
+        if (volume != null && volume.profile != null)
+        {
+            volume.profile.TryGet(out colorAdjustments);
+            volume.profile.TryGet(out GameColor);
+        }
+    }
+
+    void Update()
+    {
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (colorAdjustments != null && GameColor != null)
+        {
+            if (currentSceneIndex == 3)
+            {
+                colorAdjustments.active = true;
+                GameColor.active = false;
+            }
+            else
+            {
+                colorAdjustments.active = false;
+                GameColor.active = true;
+            }
+        }
+    }
+}
