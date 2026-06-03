6c331dc9d6cb7f282e8a4adfb41ec17a73517ade
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2026-06-02 22:39:27 -0300

    nossoplayerbugatudoasanimaçõesquandoentraemcutsceneeuficolouquito

---
+++ Procedural animation test/Assets/Scripts/Cutscenes/CutsceneTriggers.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CutsceneTriggers : MonoBehaviour
+{
+    public GameObject Cut;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) Cut.SetActive(true);
+        Destroy(gameObject);
+    }
+}
