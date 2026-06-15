7a40e20ac815dfcdec4fe60e812c47aa67d97cb2
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2026-06-14 23:39:32 -0300

    TEm de merda, e tem de jesus (amo jesus)

--- Procedural animation test/Assets/Scripts/DeathByColider.cs
+++ Procedural animation test/Assets/Scripts/DeathByColider.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class DeathByColider : MonoBehaviour
 {
+    DeathScreenAni Death;
+    public GameObject Player;
+    public GameObject SplashVfx;
+    public VisualEffect ploftVfx;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Death =  FindAnyObjectByType<DeathScreenAni>();
         
     }
 
@@ -15,7 +21,15 @@ public class DeathByColider : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player")) PlayerStats.IsDead=true;
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SplashVfx.transform.position = new Vector3(Player.transform.position.x,Player.transform.position.y+0.02f,Player.transform.position.z);
+            ploftVfx.SendEvent("OnPlay");
+            Death.Killer();
+            
+            Death.ShowDeathScreen();
+        } 
+        
         
     }
 }
