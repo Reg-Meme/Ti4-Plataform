cab39128fbb9370ebb4d1a09d7f95b529f1b5261
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2026-06-21 22:58:51 -0300

    supernatural ( a série lá)

---
+++ Procedural animation test/Assets/Scripts/CheckpointCutEvent.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CheckpointCutEvent : MonoBehaviour
+{
+    public GameObject[] Visual;
+    bool takePoint = false;
+    public Transform Checkpoint;
+    public void OnDestroy()
+    {
+        Visual[0].SetActive(true);
+        Visual[1].SetActive(true);
+        Visual[2].SetActive(true);
+        Visual[3].SetActive(true);
+        if (!takePoint)
+        {
+         takePoint = true;
+         GameBroadcast.CheckPointSave(Checkpoint.position);   
+        }
+                
+    }
+}
